a35eb612f72c13ff9ad900faa2ca407c2fce4353
SybrenVanGastel <r0785524@student.thomasmore.be>
2021-10-15 11:16:56 +0200

    initial commit

---
+++ weapon-service/src/main/java/be/itf/weaponservice/WeaponServiceApplication.java
@@ -0,0 +1,13 @@
+package be.itf.weaponservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeaponServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeaponServiceApplication.class, args);
+	}
+
+}
